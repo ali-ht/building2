35949698f91a31291d150c602ed45137de600e3f
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-08-17 00:30:51 +0330

    implement with rich tree view and tree Item 2

--- src/components/TreeView/BuildingTreeView.jsx
+++ src/components/TreeView/BuildingTreeView.jsx
@@ -1,142 +1,185 @@
 import * as React from 'react';
-import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
-import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import { styled } from '@mui/material/styles';
+import FolderRounded from '@mui/icons-material/FolderRounded';
 import MailIcon from '@mui/icons-material/Mail';
-import FolderIcon from '@mui/icons-material/Folder';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import './BuildingTreeView.css';
-import { styled } from '@mui/material';
+import { unstable_useTreeItem2 as useTreeItem2 } from '@mui/x-tree-view/useTreeItem2';
+import {
+  TreeItem2Checkbox,
+  TreeItem2Content,
+  TreeItem2IconContainer,
+  TreeItem2Label,
+  TreeItem2Root,
+} from '@mui/x-tree-view/TreeItem2';
+import { TreeItem2Provider } from '@mui/x-tree-view/TreeItem2Provider';
+import Collapse from '@mui/material/Collapse';
+import { animated, useSpring } from '@react-spring/web';
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
 
 const data = {
   id: 'root',
   title: 'پروژه ها',
-  icon: FolderIcon,
   children: [
     {
       id: '65cf312d52c2c25b64f56910',
       title: 'ساختمان',
-      icon: FolderIcon,
       children: [
         {
           id: '65cf317a52c2c25b64f5691c',
           title: 'وب اپلیکیشن',
-          icon: InsertDriveFileIcon,
-          children: []
+          children: [],
         },
         {
           id: '65cf318f52c2c25b64f56920',
           title: 'حل مسائل',
-          icon: FolderIcon,
           children: [
             {
               id: '65cf31b652c2c25b64f56924',
               title: 'مسئله شماره یک و دو',
-              icon: InsertDriveFileIcon,
-              children: []
-            }
-          ]
-        }
-      ]
+              children: [],
+            },
+          ],
+        },
+      ],
     },
     {
       id: '65cf315252c2c25b64f56914',
       title: 'به سوی ظهور',
-      icon: FolderIcon,
       children: [
         {
           id: '65cf31df52c2c25b64f56928',
           title: 'آخرالزمان',
-          icon: InsertDriveFileIcon,
-          children: []
-        }
-      ]
+          children: [],
+        },
+      ],
     },
     {
       id: '65cf320f52c2c25b64f5692c',
       title: 'بدون زیر پروژه',
-      icon: MailIcon,
-      children: []
-    }
+      children: [],
+    },
   ],
 };
 
-const StyledTreeItem = styled(TreeItem)(({ theme }) => ({
-  '& .MuiCollapse-root': {
-    paddingLeft: '0',
-    paddingRight: '12px',
+const StyledTreeItemRoot = styled(TreeItem2Root)(({ theme }) => ({
+  position: 'relative',
+}));
+
+const CustomTreeItemContent = styled(TreeItem2Content)(({ theme }) => ({
+  flexDirection: 'row-reverse',
+  borderRadius: '12px',
+  marginBottom: theme.spacing(0.5),
+  marginTop: theme.spacing(0.5),
+  padding: theme.spacing(0.5),
+  paddingRight: theme.spacing(1),
+  fontWeight: 500,
+  '&:hover': {
+    backgroundColor: '#FEEFEA',
   },
-  '& .Mui-focused': {
-    backgroundColor: '#FEEFEA !important',
-    border: 'solid 0.15rem #F15F2B !important',
-  },
-  '& .Mui-selected': {
-    backgroundColor: '#FEEFEA !important',
-  },
-  '& .MuiTreeItem-content': {
-    backgroundColor: '#F3F3F3',
-    borderRadius: '2rem !important',
-    height: '3rem',
-    padding: '1rem',
-    margin: '0.6rem 0',
+  [`&.Mui-focused, &.Mui-selected, &.Mui-selected.Mui-focused`]: {
+    backgroundColor: '#FEEFEA',
     color: 'black',
-    textAlign: 'right',
-    direction: 'ltr',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
   },
-  '& MuiTreeItem-root': {
-    padding: '1rem',
-  },
-  '& .Mui-expanded': {
-    backgroundColor: '#FEEFEA !important',
-  },
-  '& .TreeBox': {
-    backgroundColor: 'white',
-    height: 'calc(100% - 32px)',
-    width: '40vh',
-    borderRadius: '2rem',
-    padding: '1rem',
-  }
-}))
+}));
+
+const AnimatedCollapse = animated(Collapse);
+
+function TransitionComponent(props) {
+  const style = useSpring({
+    to: {
+      opacity: props.in ? 1 : 0,
+      transform: `translate3d(0,${props.in ? 0 : 20}px,0)`,
+    },
+  });
+
+  return <AnimatedCollapse style={style} {...props} />;
+}
+
+function CustomLabel({ icon: Icon, children, ...other }) {
+  return (
+    <TreeItem2Label
+      {...other}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      {Icon && (
+        <Icon
+          style={{ marginRight: '8px', fontSize: '1.2rem' }}
+        />
+      )}
+      {children}
+    </TreeItem2Label>
+  );
+}
+
+const CustomTreeItem = React.forwardRef(function CustomTreeItem(props, ref) {
+  const { id, itemId, label, icon, children, ...other } = props;
+
+  const {
+    getRootProps,
+    getContentProps,
+    getIconContainerProps,
+    getCheckboxProps,
+    getLabelProps,
+    getGroupTransitionProps,
+    status,
+  } = useTreeItem2({ id, itemId, children, label, rootRef: ref });
+
+  const expandable = children && children.length > 0;
+
+  return (
+    <TreeItem2Provider itemId={itemId}>
+      <StyledTreeItemRoot {...getRootProps(other)}>
+        <CustomTreeItemContent
+          {...getContentProps({
+            className: `content ${status.expanded ? 'Mui-expanded' : ''}`,
+          })}
+        >
+          <TreeItem2IconContainer {...getIconContainerProps()}>
+            {/* Insert custom icons if necessary */}
+          </TreeItem2IconContainer>
+          <TreeItem2Checkbox {...getCheckboxProps()} />
+          <CustomLabel
+            {...getLabelProps({ icon: icon || (expandable ? FolderRounded : MailIcon) })}
+          >
+            {label}
+          </CustomLabel>
+        </CustomTreeItemContent>
+        {children && <TransitionComponent {...getGroupTransitionProps()}>{children}</TransitionComponent>}
+      </StyledTreeItemRoot>
+    </TreeItem2Provider>
+  );
+});
 
 const renderTreeItems = (node) => {
-  const IconComponent = node.icon || MailIcon;
   return (
-    <StyledTreeItem
+    <CustomTreeItem
       key={node.id}
       itemId={node.id}
-      label={
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <IconComponent style={{ marginRight: '8px' }} />
-          <span>{node.title}</span>
-        </div>
-      }
+      label={node.title}
+      icon={node.icon} // Set custom icons if needed
     >
       {node.children && node.children.length > 0 && (
         <>
-          {node.children.map(child => renderTreeItems(child))}
+          {node.children.map((child) => renderTreeItems(child))}
         </>
       )}
-    </StyledTreeItem>
+    </CustomTreeItem>
   );
 };
 
 const BuildingTreeView = () => {
-  const [selectedItemId, setSelectedItemId] = React.useState(null);
-  const [parentItemId, setParentItemId] = React.useState(null);
-
-  const handleItemClick = (itemId, parentId) => {
-    setSelectedItemId(itemId);
-    setParentItemId(parentId);
-  };
-
   return (
-    <div className='TreeBox'>
-      <SimpleTreeView>
-        {renderTreeItems(data)}
-      </SimpleTreeView>
-    </div>
+    <RichTreeView
+      items={data.children}
+      defaultExpandedItems={['65cf312d52c2c25b64f56910']}
+      defaultSelectedItems="65cf317a52c2c25b64f5691c"
+      sx={{ height: 'fit-content', flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+      slots={{ item: CustomTreeItem }}
+      getItemLabel={(item) => item.title}
+    />
+
   );
 };
 
