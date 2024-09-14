edb12aa323c6811f6cabfee8a9b15147fd7afcf6
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-09-14 23:21:27 +0330

    add correct formatting and styles

--- src/components/SeyyedTaskPage/SmaTaskPage.jsx
+++ src/components/SeyyedTaskPage/SmaTaskPage.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { Button } from "@mui/material"; 
+import { Button } from "@mui/material";
 
 const SmaTaskPage = () => {
+    const pageStyle = {
+        display: "grid",
+        placeItems: "center", 
+        width: "100vw",
+        height: "100vh",
+    };
+
     const containerStyle = {
         display: "grid",
-        gridTemplateRows: "1fr auto 2fr auto",
+        gridTemplateRows: "1fr auto 2fr auto", 
         gap: "10px",
         width: "calc(100% - 10%)",
+        maxWidth: "800px", 
         height: "calc(100% - 10%)",
+        maxHeight: "600px", 
         backgroundColor: "#fff",
         borderRadius: "20px",
     };
 
     const grayBoxStyle = {
-        backgroundColor: "#ccc",
+        backgroundColor: "#ccc", 
         width: "100%",
         height: "100%",
         borderTopLeftRadius: "20px", 
@@ -29,7 +38,7 @@ const SmaTaskPage = () => {
 
     const tableStyle = {
         height: "100%",
-        overflowY: "auto",
+        overflowY: "auto", 
         padding: "10px",
     };
 
@@ -41,25 +50,27 @@ const SmaTaskPage = () => {
     };
 
     return (
-        <div style={containerStyle}>
-            <div style={grayBoxStyle}></div>
+        <div style={pageStyle}>
+            <div style={containerStyle}>
+                <div style={grayBoxStyle}></div>
 
-            <div style={titleBarStyle}>
-                <h3>عنوان</h3>
-                <div>
-                    <Button variant="outlined" color="secondary" style={{ marginRight: "10px" }}>ویرایش</Button>
-                    <Button variant="contained" color="error">حذف</Button>
+                <div style={titleBarStyle}>
+                    <h3>عنوان</h3>
+                    <div>
+                        <Button variant="outlined" color="secondary" style={{ marginRight: "10px" }}>ویرایش</Button>
+                        <Button variant="contained" color="error">حذف</Button>
+                    </div>
                 </div>
-            </div>
 
-            <div style={tableStyle}>
-                <p>اون جدوله</p>
-            </div>
+                <div style={tableStyle}>
+                    <p>اون جدوله</p>
+                </div>
 
-            <div style={customButtonStyle}>
-                <Button variant="contained" color="warning" fullWidth>
-                    انجام کار
-                </Button>
+                <div style={customButtonStyle}>
+                    <Button variant="contained" color="warning" fullWidth>
+                        انجام کار
+                    </Button>
+                </div>
             </div>
         </div>
     );
