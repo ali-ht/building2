26e71e121db662996128c2db52f7e91e2e70623b
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-09-14 22:59:56 +0330

    add container in center of page

--- src/components/SeyyedTaskPage/SmaTaskPage.jsx
+++ src/components/SeyyedTaskPage/SmaTaskPage.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 const SmaTaskPage = () => {
+    const containerStyle = {
+        display: "grid",
+        placeItems: "center",
+        width: "100vw",
+        height: "100vh",
+    };
+
+    const styledBox = {
+        width: "calc(100% - 10%)",
+        height: "calc(100% - 10%)", 
+        backgroundColor: "#fff",
+        borderRadius: "20px",
+    };
+
     return (
-        <div></div>
-    )
+        <div style={containerStyle}>
+            <div style={styledBox}></div>
+        </div>
+    );
 }
 
 export default SmaTaskPage;
