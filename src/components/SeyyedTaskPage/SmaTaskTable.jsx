f636f07cafbbf8d28658d5f2db94158817c6af0a
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-09-14 23:40:33 +0330

    create task table component

---
+++ src/components/SeyyedTaskPage/SmaTaskTable.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+    Table, TableBody, TableCell, TableContainer, TableRow, Paper,
+    Typography
+} from "@mui/material";
+import LayersIcon from '@mui/icons-material/Layers'; // Icon for طبقه
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline'; // Icon for پروژه
+import PersonIcon from '@mui/icons-material/Person'; // Icon for تعریف کننده
+import DescriptionIcon from '@mui/icons-material/Description'; // Icon for توضیحات
+import NoteAddIcon from '@mui/icons-material/NoteAdd'; // Icon for پیش نیاز
+import AttachFileIcon from '@mui/icons-material/AttachFile'; // Icon for پیوست
+
+const SmaTaskTable = () => {
+    const rows = [
+        { label: 'طبقه', value: '۷', icon: <LayersIcon color="secondary" /> },
+        { label: 'پروژه', value: 'بعثت تا ظهور / اسب‌سوار', icon: <WorkOutlineIcon color="secondary" /> },
+        { label: 'تعریف کننده', value: 'دکتر', icon: <PersonIcon color="secondary" /> },
+        { label: 'توضیحات', value: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از ...', icon: <DescriptionIcon color="secondary" /> },
+        { label: 'پیش نیاز', value: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از ...', icon: <NoteAddIcon color="secondary" /> },
+        { label: 'پیوست', value: 'productspec.word', icon: <AttachFileIcon color="secondary" /> },
+    ];
+
+    return (
+        <TableContainer component={Paper} style={{ boxShadow: "none" }}>
+            <Table aria-label="simple table">
+                <TableBody>
+                    {rows.map((row, index) => (
+                        <TableRow key={index}>
+                            {/* First Column - Icon */}
+                            <TableCell align="center" style={{ borderBottom: "none" }}>
+                                {row.icon}
+                            </TableCell>
+                            {/* Second Column - Label */}
+                            <TableCell align="right" style={{ borderBottom: "none" }}>
+                                <Typography>{row.label}</Typography>
+                            </TableCell>
+                            {/* Third Column - Value */}
+                            <TableCell align="left" style={{ borderBottom: "none" }}>
+                                <Typography>{row.value}</Typography>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+export default SmaTaskTable;
